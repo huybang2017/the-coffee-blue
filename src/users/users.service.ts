fe6dcf3a7af280f0b71076a3c2baf3f18edb8fbf
huybang2017 <ndhbang2017@gmail.com>
2025-04-08 04:14:31 +0700

    feat: add service user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,38 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
 
-  async create(email: string, password: string) {
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const { password, ...rest } = createUserDto;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = this.userRepo.create({ email, password: hashedPassword });
+    const user = this.userRepo.create({ ...rest, password: hashedPassword });
     return this.userRepo.save(user);
   }
 
-  findAll() {
-    return this.userRepo.find();
+  async findAll(): Promise<User[]> {
+    return this.userRepo.find({ relations: ['role'] });
   }
 
-  findOne(id: number) {
-    return this.userRepo.findOneBy({ id });
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepo.findOne({
+      where: { id },
+      relations: ['role'],
+    });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
   }
 
-  async update(id: number, email: string) {
-    await this.userRepo.update(id, { email });
-    return this.findOne(id);
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.findOne(id);
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
+
+    Object.assign(user, updateUserDto);
+    return this.userRepo.save(user);
   }
 
-  async remove(id: number) {
-    await this.userRepo.delete(id);
+  async remove(id: number): Promise<{ deleted: boolean }> {
+    const result = await this.userRepo.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
     return { deleted: true };
   }
 
-  findByEmail(email: string) {
-    return this.userRepo.findOneBy({ email });
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepo.findOne({ where: { email }, relations: ['role'] });
   }
 }
