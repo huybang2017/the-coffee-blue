d8251c435bc07db0454ca7cd2585dc127556b180
huybang2017 <ndhbang2017@gmail.com>
2025-04-08 04:14:22 +0700

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,11 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Role } from 'src/roles/entities/role.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
-@Entity({
-  name: 'users',
-})
+@Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ name: 'full_name', nullable: true })
+  fullName: string;
 
   @Column({ unique: true })
   email: string;
@@ -13,6 +23,25 @@ export class User {
   @Column()
   password: string;
 
-  @Column({ default: 'user' })
-  role: string;
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  address: string;
+
+  @Column({ name: 'role_id', nullable: true })
+  roleId: number;
+
+  @ManyToOne(() => Role, (role) => role.users)
+  @JoinColumn({ name: 'role_id' })
+  role: Role;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    comment: 'Timestamp when user data was last updated',
+  })
+  updatedAt: Date;
 }
