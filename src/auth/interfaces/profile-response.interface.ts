e75393b8c424b656d09bc22e23620b41379fc00c
huybang2017 <ndhbang2017@gmail.com>
2025-04-11 23:42:25 +0700

    feat: add profile response

---
+++ src/auth/interfaces/profile-response.interface.ts
@@ -0,0 +1,10 @@
+export interface ProfileResponse {
+  id: number;
+  fullName: string;
+  email: string;
+  phone: string;
+  address: string;
+  roleId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
