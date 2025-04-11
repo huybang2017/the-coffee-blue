3bada8e894c60b1372d6856f212279547cdd1b12
huybang2017 <ndhbang2017@gmail.com>
2025-04-11 23:43:36 +0700

    feat: add success filter

---
+++ src/utils/exception-filter/success.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class SuccessInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: 200,
+        message: 'Success',
+        data,
+      })),
+    );
+  }
+}
